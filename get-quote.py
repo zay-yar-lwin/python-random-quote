6df63e6ca7d84870efdaa7c363ddcbdf2cd4239f
Zayar Lwin <zyl.pol.mm.81298@outlook.com>
2021-11-23 19:02:35 +0630

    Random quote bot is updated

--- get-quote.py
+++ get-quote.py
@@ -8,9 +8,11 @@ def setup():
   quotes = f.readlines()
   f.close()
   
-  last = 13
-  rnd = random.randint (0, last)
-  print(quotes[rnd])
+  last = 17
+  rnd1 = random.randint (0, last)
+  rnd2 = random.randint (0, last)
+  print(quotes[rnd1], end = " ")
+  print(quotes[rnd2], end = " ")
 
 if __name__== "__main__":
   setup()
